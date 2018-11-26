cbd828d71d2f59db9c29c2e3614d8fae05e9867e
MohamedLazar <37117998+MohamedLazar@users.noreply.github.com>
2018-11-26 20:52:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
+
+//Modules
 const program = require('commander')
 const inquirer = require('inquirer')
 
+//Initialisation des variables
 var api = require('marvel-api')
 var marvel = api.createClient({
   publicKey: 'aca2d23f5a7b7daad9c7b61f1069afa6',
   privateKey: 'e7f4caec2d1d2ab015e90fe14f6f0485b74212ac'
 })
 
+//On enumere les options
 program
   .version('1.0.0')
   .option('-c, --character', 'Affiche un personnage')
   .option('-a --all', 'Afficher tous les personnages')
 program.parse(process.argv)
 
+//MAIN
 if (program.character) {
   inquirer.prompt([
     {
